a91c936cbbd7d627beb1e1807d96cc1073f67547
Scott Downie <scott.downie@live.com>
2017-11-14 14:45:04 +0000

    Adding the parsing from stdin and adding the conversion back to base20 from decimal. Submission passed on CodinGame 100%

--- ClassicMedium/MayanCalculation/core.go
+++ ClassicMedium/MayanCalculation/core.go
@@ -1,8 +1,8 @@
 // NUMERICAL SYSTEM
 // Uses base 20
-// Each numeral is an LxH pattern consisting of the character set .-o
+// Each numeral is an LxH pattern consisting of the character set ._o
 // There are 20 numbers ranging from 0-19
-// A number is a vertical column of numerals where each numeral is multiplied by 20 to the power of position and added to the next one (top to bottom) => (X x 20^2) + (Y x 20^1) + (Z x 20^0). The top most number is the most significant
+// A number is a vertical column of numerals where the top most number is the most significant (i.e. the last numeral is the unit column, the one to the left is the 20 column, etc)
 // Numeral patterns change with each test
 
 // INPUT
@@ -12,7 +12,7 @@
 // 4. S1 lines representing the first number - each line has L characters
 // 5. The number of lines S2 for the second number
 // 6. S2 lines representing the second number - each line has L characters
-// 7. The operation to perform between the 2 numbers (*, /, +, -)
+// 7. The operation to perform between the 2 numbers (*, /, +, _)
 
 // OUTPUT
 // The result of the operation in mayan H lines per section with each line having length L (i.e. the number is split across multiple lines)
@@ -20,13 +20,14 @@
 // NOTES
 // L and H between 0 and 100 (excl)
 // S between 0 and 1000 (excl)
-// Mayan numbers will not exceed 2 ^ 63 (64-bit signed)
+// Numbers will not exceed 2 ^ 63 (64-bit required)
 
 package main
 
 import "fmt"
 import "os"
 import "math"
+import "errors"
 
 /// CodinGame reserves stdout for returning answers and recommends
 /// stderr for debug logging
@@ -35,32 +36,67 @@ func debugLog(msg interface{}) {
     fmt.Fprintln(os.Stderr, msg)
 }
 
-/// Convert the given integer number to a sequence of mayan numeral patterns. where each patter simply using the index
+/// Convert the given decimal integer number to a sequence of mayan numeral patterns with the first numeral being the most significant
 /// Requires that the 20 mayan numerals are in ascending order 0 - 19
 ///
-func base10to20(toConvert int64, mayanNumerals []string) string {
+func base10to20(toConvert int64, mayanNumerals []string) []string {
 
-    //toConvert
-    return mayanNumerals[toConvert]
+    //Handling zero as a special case as log10 0 is undefined but can also
+    //be used to speed up any of the single unit cases
+    if toConvert < 20 {
+        return []string{mayanNumerals[toConvert]}
+    }
+
+    numDigits := int(math.Floor(math.Log10(math.Abs(float64(toConvert))))) + 1
+
+    output := make([]string, numDigits)
+
+    //We need to remove leading zeros. This will track where our first number is
+    startIndex := -1
+
+    //For example the value 21 is 1 unit and 1 20 so the mayan value returned will be 1,1
+    //In order to carry the remainder we need to start from the most significant column
+    remaining := toConvert
+    for i:=0; i<numDigits; i++ {
+        denom := int64(math.Pow(20, float64(numDigits - i - 1)))
+        numInCol := remaining/denom
+        output[i] = mayanNumerals[numInCol]
+        if numInCol > 0 && startIndex < 0 {
+            startIndex = i
+        }
+        remaining = remaining % denom
+    }
+
+    //Trim the start
+    return output[startIndex:]
 }
 
-/// Convert the given mayan number to an integer. The mayan number comprises several mayan numerals in base 20 with the first being the most
+/// Convert the given mayan number to a decimal integer. The mayan number comprises several mayan numerals in base 20 with the first being the most
 /// significant. The number is converted using the formula (X0 x 20^N) + (X1 x 20^1) + (XN x 20^0). Each mayan numeral pattern is converted
-/// to an integer simply using the index so requires that the 20 mayan numerals are in ascending order 0 - 19
+/// to an decimal value simply using the index so requires that the 20 mayan numerals are in ascending order 0 - 19
 ///
-func base20to10(toConvert []string, mayanNumerals []string) int64 {
-    var total int64 = 0
+func base20to10(toConvert []string, mayanNumerals []string) (total int64, err error) {
+    total = 0
+    err = nil
 
-    for in, n := range mayanNumerals {
-        for ic, c := range toConvert {
+    for ic, c := range toConvert {
+        found := false
+        for in, n := range mayanNumerals {
             if n == c {
                 power := len(toConvert) - ic - 1
                 total += int64(in) * int64(math.Pow(20, float64(power)))
+                found = true
+                break
             }
+        }
+
+        if found == false {
+            err = errors.New("Cannot find numeral")
+            return
         }
     }
 
-    return total
+    return
 }
 
 /// CodinGame feeds us a number as a series of numerals ordered vertically. We get the number
@@ -81,23 +117,28 @@ func parseSplitBase20Number(lines []string, numeralWidth int) []string {
 /// the given numerals for base10 conversion
 ///
 func run(numeralWidth int, numeralsLines []string, aLines []string, bLines []string, operator string) {
-
     // Split the lines along the numeral boundaries into individual Mayan numeral strings - lowest first
     var numerals [20]string
     for lineIdx:=0; lineIdx<len(numeralsLines); lineIdx++ {
-        for charIdx:=0; charIdx<20; charIdx++ {
+        for charIdx:=0; charIdx<len(numeralsLines[lineIdx]); charIdx++ {
             numeralIndex := (charIdx / numeralWidth)
             numerals[numeralIndex] = numerals[numeralIndex] + string(numeralsLines[lineIdx][charIdx])
         }
     }
 
-    // Split the operands into individual Mayan numerals - most significant first
+    // Split the operand numbers into individual Mayan numerals - most significant first
     a20 := parseSplitBase20Number(aLines, numeralWidth)
     b20 := parseSplitBase20Number(bLines, numeralWidth)
 
     // Convert from Mayan to base 10 ints for math operations
-    var a10 = base20to10(a20, numerals[:])
-    var b10 = base20to10(b20, numerals[:])
+    a10, err := base20to10(a20, numerals[:])
+    if err != nil {
+        panic(err)
+    }
+    b10, err := base20to10(b20, numerals[:])
+    if err != nil {
+        panic(err)
+    }
 
     var result10 int64 = 0
     switch operator {
@@ -114,59 +155,102 @@ func run(numeralWidth int, numeralsLines []string, aLines []string, bLines []str
     // Convert back to base 20 and then to the Mayan numerals 
     result20 := base10to20(result10, numerals[:])
     
+    debugLog(numerals)
     debugLog(fmt.Sprintf("%s %s %s => %d %s %d = %d => %s", a20, operator, b20, a10, operator, b10, result10, result20))
 
     // Send the answer to CodinGame split into lines
-    line := 0
-    for i:=0; i<len(numeralsLines); i++ {
-
-        fmt.Println(result20[line:line+numeralWidth])
-        line += numeralWidth
+    for i:=0; i<len(result20); i++ {
+        line := 0
+        for j:=0; j<len(result20[i])/numeralWidth; j++ {
+            fmt.Println(result20[i][line:line+numeralWidth])
+            line += numeralWidth
+        }
     }
 }
 
-
+/// Used by CodinGame to feed us the input via stdin
+///
 func main() {
-    // L and H are the width and height of each numeral pattern e.g. 
+    // L and H are the width (length) and height of each numeral pattern e.g. 
     // .oo.
     // o..o
     // .oo.
     // ....
     //
     // Has a L and H of 4
-    // var L, H int
-    // fmt.Scan(&L, &H)
-    
-    // for i := 0; i < H; i++ 
-    // {
-    //     var numeral string
-    //     fmt.Scan(&numeral)
-    // }
-    // var S1 int
-    // fmt.Scan(&S1)
-    
-    // for i := 0; i < S1; i++ 
-    // {
-    //     var num1Line string
-    //     fmt.Scan(&num1Line)
-    // }
-    // var S2 int
-    // fmt.Scan(&S2)
-    
-    // for i := 0; i < S2; i++ 
-    // {
-    //     var num2Line string
-    //     fmt.Scan(&num2Line)
-    // }
-    // var operation string
-    // fmt.Scan(&operation)
-    
-    
-    // // fmt.Fprintln(os.Stderr, "Debug messages...")
-    // fmt.Println("result")// Write answer to stdout
+    var l, h int
+    fmt.Scan(&l, &h)
 
-    a := []string{"o...", "....", "....", "...."}
-    b := []string{"o...", "....", "....", "...."}
-    numerals := []string{".oo.o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo", "o..o................____________________________________________________________", ".oo.....................................________________________________________", "............................................................____________________"}
-    run(4, numerals, a, b, "+")
+    // The list of Mayan numerals are sent horizontally by scan lines (top to bottom)
+    numerals := make([]string, h)
+    for i:=0; i<h; i++ {
+        var line string
+        fmt.Scan(&line)
+        numerals[i] = line
+    }
+
+    // The height of the first operand in lines
+    var s1 int
+    fmt.Scan(&s1)
+
+    // The number is then scanned in lines top to bottom
+    a := make([]string, s1)
+    for i:=0; i<s1; i++ {
+        var line string
+        fmt.Scan(&line)
+        a[i] = line
+    }
+
+    // The height of the second operand in lines
+    var s2 int
+    fmt.Scan(&s2)
+
+    // The number is then scanned in lines top to bottom
+    b := make([]string, s2)
+    for i:=0; i<s2; i++ {
+        var line string
+        fmt.Scan(&line)
+        b[i] = line
+    }
+
+    // The operation
+    var operation string
+    fmt.Scan(&operation)
+
+    //TEST 01 - Simple addition
+    // a = []string{"o...", "....", "....", "...."}
+    // b = []string{"o...", "....", "....", "...."}
+    // numerals = []string{".oo.o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo", "o..o................____________________________________________________________", ".oo.....................................________________________________________", "............................................................____________________"}
+    // operation = "+"
+    // l = 4
+
+    //TEST 02 - Carry addition
+    // a = []string{"ooo.", "____", "____", "____"}
+    // b = []string{"ooo.", "....", "....", "...."}
+    // numerals = []string{".oo.o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo", "o..o................____________________________________________________________", ".oo.....................................________________________________________", "............................................................____________________"}
+    // operation = "+"
+    // l = 4
+
+    //TEST 05 - Subtraction
+    // a = []string{"o...","____","....","....","ooo.","....","....","...."}
+    // b = []string{"oo..","____","....","...."}
+    // numerals = []string{".oo.o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo", "o..o................____________________________________________________________", ".oo.....................................________________________________________", "............................................................____________________"}
+    // operation = "-"
+    // l = 4
+
+    //TEST 05 - Great Multiplication
+    // a = []string{"o...","....","....","....","....","____","____","....","oo..","____","____","____","....","____","....","...."}
+    // b = []string{"oooo","....","....","....","ooo.","____","____","____","oo..","____","____","....","....","____","____","....","oo..","____","____","...."}
+    // numerals = []string{".oo.o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo", "o..o................____________________________________________________________", ".oo.....................................________________________________________", "............................................................____________________"}
+    // operation = "*"
+    // l = 4
+
+    //TEST 09 - Zero
+    // a = []string{"....","____","____","____","ooo.","____","....","....","oo..","____","____","....","o...","____","____","____"}
+    // b = []string{".oo.","o..o",".oo.","...."}
+    // numerals = []string{".oo.o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo....o...oo..ooo.oooo", "o..o................____________________________________________________________", ".oo.....................................________________________________________", "............................................................____________________"}
+    // operation = "*"
+    // l = 4
+
+    run(l, numerals, a, b, operation)
 }
